ac5b0ad8ae8575ffde9cb10bae3ffc8011ed6fb4
jorgeagui <aguilera.go.jorge@gmail.com>
2020-09-22 14:09:50 +0200

    common costants

---
+++ src/main/java/clothingstorefranchise/spring/common/constants/EventState.java
@@ -0,0 +1,10 @@
+package clothingstorefranchise.spring.common.constants;
+
+public final class EventState {
+	public static final int NO_PUBLISHED = 0;
+	public static final int IN_PROGRESS = 1;
+	public static final int PUBLISHED = 2;
+	public static final int PUBLISHED_FAILED = 3;
+	
+	private EventState() { }
+}
---
+++ src/main/java/clothingstorefranchise/spring/common/constants/Rol.java
@@ -0,0 +1,9 @@
+package clothingstorefranchise.spring.common.constants;
+
+public final class Rol {
+	
+	public static final String ADMIN = "ADMIN";
+	public static final String CUSTOMER = "CUSTOMER";
+	
+	private Rol() { }
+}
