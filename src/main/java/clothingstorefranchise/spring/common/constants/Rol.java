06df2f5f103ff13619043f955f5b63557222a6cf
jorgeagui <aguilera.go.jorge@gmail.com>
2020-12-21 12:00:37 +0100

    Added employee roles

--- src/main/java/clothingstorefranchise/spring/common/constants/Rol.java
+++ src/main/java/clothingstorefranchise/spring/common/constants/Rol.java
@@ -4,6 +4,8 @@ public final class Rol {
 	
 	public static final String ADMIN = "ADMIN";
 	public static final String CUSTOMER = "CUSTOMER";
+	public static final String SHOP_EMPLOYEE = "SHOP_EMPLOYEE";
+	public static final String WAREHOUSE_EMPLOYEE = "WAREHOUSE_EMPLOYEE";
 	
 	private Rol() { }
 }
--- src/main/java/clothingstorefranchise/spring/common/security/filter/JwtTokenAuthorizationFilter.java
+++ src/main/java/clothingstorefranchise/spring/common/security/filter/JwtTokenAuthorizationFilter.java
@@ -7,7 +7,6 @@ import clothingstorefranchise.spring.common.security.token.TokenConverter;
 import clothingstorefranchise.spring.common.security.util.SecurityContextUtil;
 import lombok.RequiredArgsConstructor;
 import lombok.SneakyThrows;
-import lombok.extern.slf4j.Slf4j;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.lang.NonNull;
